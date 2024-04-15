f802d446f8679a907141d9d3e999f0f175615c7a
ono212 <qkrwjdals302@naver.com>
2024-04-16 00:10:17 +0900

    feat: 접근성 기반, 서버 사이드  form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,17 +5,31 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'customer를 선택하세요.',
+  }),
+  amount: z.coerce.number().gt(0, { message: '0$보다 큰 수 를 입력하세요.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'invoice 상태를 선택하세요.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
   /* CreateInvoice 사용하기 전
   // formData값 추출하기
   const rawFormData = {
@@ -27,35 +41,44 @@ export async function createInvoice(formData: FormData) {
   console.log(rawFormData);
   */
 
-  const { customerId, amount, status } = CreateInvoice.parse({
+  // Zod를 사용하여 form Validate
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   // 센트 단위로 저장하기 위해서
   const amountInCents = amount * 100;
-
   // YYYY-MM-DD 형식으로 날짜 생성
   const date = new Date().toISOString().split('T')[0];
-
   // ----------⬆️ validation 끝----------
 
   // 데이터베이스에 validation한 데이터 삽입
   try {
     await sql`
-  INSERT INTO invoices (customer_id, amount, status, date)
-  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-`;
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
   } catch (error) {
+    // If a database error occurs, return a more specific error.
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
   }
 
-  // 데이터가 바뀌었기 때문에 캐시 삭제 및 서버에 새롭게 요청하기. => revalidatePath함수를 사용하면 됨
+  // Revalidate the cache for the invoices page and redirect the user.  // 데이터가 바뀌었기 때문에 캐시 삭제 및 서버에 새롭게 요청하기. => revalidatePath함수를 사용하면 됨
   revalidatePath('/dashboard/invoices');
-
   // 현재 create페이지니까 invoice페이지로 리다이렉션
   redirect('/dashboard/invoices');
 }
@@ -63,21 +86,33 @@ export async function createInvoice(formData: FormData) {
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
     await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-      `;
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
   } catch (error) {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
@@ -87,7 +122,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
+  // throw new Error('Failed to Delete Invoice');
 
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
